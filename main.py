108394c13aa7184eeaf204edb1c5d8e367c17425
Heinrich <freelanceharms@gmail.com>
2022-01-23 22:09:41 -0600

    watermark insertion working, although visible distortion is present

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 from math import exp, sqrt
+from heapq import nlargest
+from itertools import product
+
+from scipy.fft import dctn, idctn
+from numpy import array, ndarray, dot, uint8
+from numpy.random import randn
 
 from typing import Callable
 
-from numpy import ndarray, float64, dot
-from numpy.random import randn
-
 
 def formula1(
-    v: float64,
-    x: float64,
+    v: float,
+    x: float,
     alpha: float
 ):
     return v + alpha * x
 
 
 def formula2(
-    v: float64,
-    x: float64,
+    v: float,
+    x: float,
     alpha: float
 ):
     return v * (1 + alpha * x)
 
 
 def formula3(
-    v: float64,
-    x: float64,
+    v: float,
+    x: float,
     alpha: float
 ):
     return v * exp(alpha * x)
@@ -42,11 +45,43 @@ def insert_watermark(
     image: ndarray,
     watermark: ndarray,
     alpha: float,
-    insertion_formula: Callable = formula2
-):
-    pass
+    insertion_function: Callable = formula2,
+) -> ndarray:
+
+    # ensure image dtype is set to float
+    if image.dtype != float:
+        image = image.astype(float)
+
+    # compute transform domain of image
+    transform_domain = dctn(image)
+
+    # locate most significant coefficients
+    coefficients = nlargest(
+        len(watermark),
+        list(product(
+            range(image.shape[0]),
+            range(image.shape[1]),
+            range(image.shape[2])
+        ))[1:],
+        transform_domain.__getitem__)
+
+    # insert watermark using specified insertion function
+    for (j, k, l), xi in zip(coefficients, watermark):
+        transform_domain[j, k, l] = insertion_function(transform_domain[j, k, l], xi, alpha)
+
+    # return reconstructed and quantized spatial domain
+    return idctn(transform_domain).astype(uint8)
 
 
 if __name__ == "__main__":
 
-    from PIL import Image
+    from sys import argv
+    from cv2 import imread, imwrite
+
+    watermark = generate_watermark(4096)
+
+    img = imread(argv[1])
+
+    watermarked = insert_watermark(img, watermark, 0.02)
+
+    imwrite(argv[2], watermarked)
